68dcfc883b485552978d92e5788efa8759267147
WalterGiraldo <waltergiraldo@kubesoft.com>
2022-11-14 17:38:48 -0500

    cambios iniciales 14/11

---
+++ src/Description/Description.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Description.css'
+
+export default function Descripcion (){
+
+    return (
+        <>
+        <main>
+            <h1>Hola</h1>
+        </main>
+        </>
+    )
+}
