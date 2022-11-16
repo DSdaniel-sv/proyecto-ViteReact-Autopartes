c0f50bbc5b37ceed448bd2c21f0e1c9d9279239d
michaelkevin1 <kevinmorillo60@gmail.com>
2022-11-15 23:24:24 -0500

    ofertas-css

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import Inicio from '../pages/Inicio';
 import Contacto from '../pages/Contacto';
 import Footer from '../components/Footer'
+import Ofertas from '../pages/Ofertas';
 
 export default function Router() {
   return (
@@ -12,6 +13,7 @@ export default function Router() {
         <Routes>
             <Route path="/" element={<Inicio/>}/>
             <Route path="Contacto" element={<Contacto/>}/>
+            <Route path='ofertas' element={<Ofertas/>}/>
         </Routes>
         <Footer/>
     </BrowserRouter>
--- src/pages/Ofertas.jsx
+++ src/pages/Ofertas.jsx
@@ -1,1 +1,74 @@
-// 
+// Page-Ofertas
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../pages/Ofertas.css'
+
+export default function Ofertas() {
+  return (
+
+
+    <div>
+
+        <div className='ofertas-title'>
+            <h2>OFERTAS EXCLUSIVAS</h2>
+        </div>
+    <div className='container'>
+        {/* Ofertas */}
+        <div className='row row-cols-1 row-cols-md-2 g-4'>
+            {/* Oferta-1 */}
+            <div className="col">
+                <div className="card">
+                    <Link to='/'>
+                        <img src="./src/assets/img-multimedia/ofertas/oferta-1.png" className="card-img-top" alt="Oferta-1"/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Oferta Pack Nissan</h5>    
+                    </div>
+                </div>
+            </div>
+            {/* Oferta-2 */}
+            <div classNames="col">
+                <div className="card">
+                    <Link to='/'>
+                        <img src="./src/assets/img-multimedia/ofertas/oferta-2.png" className="card-img-top" alt="Oferta-2"/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Oferta pack Toyota</h5>
+                    </div>
+                </div>
+            </div>
+            {/* Oferta-3 */}
+            <div className="col">
+                <div className="card">
+                    <Link to='/'>
+                        <img src="./src/assets/img-multimedia/ofertas/ofertas-3.png" className="card-img-top" alt="Oferta-3"/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Combo de lubricaión Diesel 15W40</h5>
+                    </div>
+                </div>
+            </div>
+            {/* Oferta-4 */}
+            <div className="col">
+                <div className="card">
+                    <Link to='/'>
+                        <img src="./src/assets/img-multimedia/ofertas/ofertas-4.png" className="card-img-top" alt="Oferta-4"/>
+                    </Link>
+                    <div className="card-body">
+                        <h5 className="card-title">Combro de lubricación 20W50</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* Envio-img */}
+        <div className="envios-img">
+            <figure>
+                <img src="./src/assets/img-multimedia/ofertas/envios.png" className="card-img-top" alt="Envios"/>
+            </figure>
+        </div>
+        
+    </div>
+    </div>
+  )
+}
