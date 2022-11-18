2cd694380ac1d12914fd7e70a96f8344453b25b9
WalterGiraldo <waltergiraldo@kubesoft.com>
2022-11-17 19:30:50 -0500

    cambios walter 17/11/2022

--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -4,297 +4,101 @@ import '../pages/Galery.css'
 export default function Galery() {
 
     return (
-        <>
+
+        
+        
+          <>
+          <div className="container-2"></div>
          
-             <div className="container-2">
-                <div className="sect sect__motores">
-                    <div className="box__title">
-                        <h2>MOTORES</h2>
-                    </div>
-                    <div className="box__card">
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                    </div>
+         <div className="card">
+              <div className="imgBx">
+                <img className="imgg" src="./src/assets/img-multimedia/galery-img/auto.png" alt="banner"/>
+              </div>
+              <div className="contentBx">
+                <h2 className="h2">2018 Acura RDX </h2>
+                <div className="size">
+                  <h3 className="sizeh3">Size :</h3>
+                  <span>7</span>
+                  <span>8</span>
+                  <span>9</span>
+                  <span>10</span>
                 </div>
-
-                <div className="sect sect__repuestos">
-                    <div className="box__title">
-                        <h2>REPUESTOS</h2>
-                    </div>
-                    <div className="box__card">
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png"alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                    </div>
+                <div className="color">
+                  <h3 className="colorh3">Color :</h3>
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </div>
-
-                <div className="sect sect__accesorios">
-                    <div className="box__title">
-                        <h2>ACCESORIOS</h2>
-                    </div>
-                    <div className="box__card">
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="imgBx">
-                                <img src="./src/assets/img-multimedia/galery-img/piezas.png" alt="banner"/>
-                            </div>
-                            <div className="contentBx">
-                                <h2>2018 Acura RDX </h2>
-                                <div className="size">
-                                    <h3>Size :</h3>
-                                    <span>7</span>
-                                    <span>8</span>
-                                    <span>9</span>
-                                    <span>10</span>
-                                </div>
-                                <div className="color">
-                                    <h3>Color :</h3>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                                <a href="descripcion/index.html">Buy Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <a href="descripcion/index.html">Buy Now</a>
+              </div>
             </div>
+            <div className="card">
+                <div className="imgBx">
+                  <img className="imgg" src="a_7-removebg-preview.png" alt="banner"/>
+                </div>
+                <div className="contentBx">
+                  <h2 className="h2">2018 Acura RDX </h2>
+                  <div className="size">
+                    <h3 className="sizeh3">Size :</h3>
+                    <span>7</span>
+                    <span>8</span>
+                    <span>9</span>
+                    <span>10</span>
+                  </div>
+                  <div className="color">
+                    <h3  className="colorh3">Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="descripcion/index.html">Buy Now</a>
+                </div>
+              </div>
+              <div className="card">
+                <div className="imgBx">
+                  <img className="imgg" src="44bb9450175c6bb59d5979a0d76d56fa-removebg-preview.png" alt="banner"/>
+                </div>
+                <div class="contentBx">
+                  <h2 className="h2">2018 Acura RDX </h2>
+                  <div className="size">
+                    <h3 className="sizeh3">Size :</h3>
+                    <span>7</span>
+                    <span>8</span>
+                    <span>9</span>
+                    <span>10</span>
+                  </div>
+                  <div className="color">
+                    <h3 className="colorh3">Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="descripcion/index.html">Buy Now</a>
+                </div>
+              </div>
+              <div className="card">
+                <div className="imgBx">
+                  <img className="imgg" src="a_7-removebg-preview.png" alt="banner"/>
+                </div>
+                <div className="contentBx">
+                  <h2 className="h2">2018 Acura RDX </h2>
+                  <div className="size">
+                    <h3 className="sizeh3">Size :</h3>
+                    <span>7</span>
+                    <span>8</span>
+                    <span>9</span>
+                    <span>10</span>
+                  </div>
+                  <div className="color">
+                    <h3 className="colorh3">Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="descripcion/index.html">Buy Now</a>
+                </div>
+              </div>
+        
         </>
     )
 }
