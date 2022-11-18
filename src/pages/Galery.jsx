0d4d73166149733c7f4dcb15ae2a7899b6657e93
WalterGiraldo <waltergiraldo@kubesoft.com>
2022-11-17 20:42:57 -0500

    cambios walter 17/11/2022

--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -34,7 +34,7 @@ export default function Galery() {
             </div>
             <div className="card">
                 <div className="imgBx">
-                  <img className="imgg" src="a_7-removebg-preview.png" alt="banner"/>
+                  <img className="imgg" src="./src/assets/img-multimedia/galery-img/auto.png" alt="banner"/>
                 </div>
                 <div className="contentBx">
                   <h2 className="h2">2018 Acura RDX </h2>
@@ -56,7 +56,7 @@ export default function Galery() {
               </div>
               <div className="card">
                 <div className="imgBx">
-                  <img className="imgg" src="44bb9450175c6bb59d5979a0d76d56fa-removebg-preview.png" alt="banner"/>
+                  <img className="imgg" src="./src/assets/img-multimedia/galery-img/auto.png" alt="banner" />
                 </div>
                 <div class="contentBx">
                   <h2 className="h2">2018 Acura RDX </h2>
@@ -78,16 +78,16 @@ export default function Galery() {
               </div>
               <div className="card">
                 <div className="imgBx">
-                  <img className="imgg" src="a_7-removebg-preview.png" alt="banner"/>
+                  <img className="imgg" src="./src/assets/img-multimedia/galery-img/auto.png" alt="banner"/>
                 </div>
                 <div className="contentBx">
                   <h2 className="h2">2018 Acura RDX </h2>
                   <div className="size">
                     <h3 className="sizeh3">Size :</h3>
-                    <span>7</span>
-                    <span>8</span>
-                    <span>9</span>
-                    <span>10</span>
+                    <span className="size-span">7</span>
+                    <span  className="size-span" >8</span>
+                    <span className="size-span" >9</span>
+                    <span className="size-span">10</span>
                   </div>
                   <div className="color">
                     <h3 className="colorh3">Color :</h3>
@@ -95,7 +95,7 @@ export default function Galery() {
                     <span></span>
                     <span></span>
                   </div>
-                  <a href="descripcion/index.html">Buy Now</a>
+                  <a className="a" href="descripcion/index.html">Buy Now</a>
                 </div>
               </div>
         
